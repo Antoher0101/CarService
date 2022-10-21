2ff96c412885ac5915821335306bcaa967ef6025
Antoher0101 <firelessball@yandex.ru>
2022-10-21 23:17:02 +0500

    Now the city table in the browser is updated after commit DefaultCity changes

--- modules/web/src/com/company/carservice/web/screens/city/CityBrowse.java
+++ modules/web/src/com/company/carservice/web/screens/city/CityBrowse.java
@@ -1,11 +1,21 @@
 package com.company.carservice.web.screens.city;
 
+import com.haulmont.cuba.gui.model.CollectionLoader;
 import com.haulmont.cuba.gui.screen.*;
 import com.company.carservice.entity.City;
+
+import javax.inject.Inject;
 
 @UiController("carservice_City.browse")
 @UiDescriptor("city-browse.xml")
 @LookupComponent("citiesTable")
 @LoadDataBeforeShow
 public class CityBrowse extends StandardLookup<City> {
+    @Inject
+    private CollectionLoader<City> citiesDl;
+
+    @Install(to = "citiesTable.edit", subject = "afterCommitHandler")
+    protected void customAfterCommitHandler(City city){
+        citiesDl.load();
+    }
 }
