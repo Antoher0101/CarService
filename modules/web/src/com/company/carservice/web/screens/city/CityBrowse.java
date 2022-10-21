1855bd3fc381cb66d36c9da7393662ca99face70
Antoher0101 <firelessball@yandex.ru>
2022-10-21 16:11:49 +0500

    Now the citiesTable updates its view in browse after commit

--- modules/web/src/com/company/carservice/web/screens/city/CityBrowse.java
+++ modules/web/src/com/company/carservice/web/screens/city/CityBrowse.java
@@ -1,11 +1,26 @@
 package com.company.carservice.web.screens.city;
 
-import com.haulmont.cuba.gui.screen.*;
 import com.company.carservice.entity.City;
+import com.haulmont.cuba.core.entity.Entity;
+import com.haulmont.cuba.gui.ScreenBuilders;
+import com.haulmont.cuba.gui.actions.list.EditAction;
+import com.haulmont.cuba.gui.components.GroupTable;
+import com.haulmont.cuba.gui.model.CollectionContainer;
+import com.haulmont.cuba.gui.model.CollectionLoader;
+import com.haulmont.cuba.gui.screen.*;
+
+import javax.inject.Inject;
+import javax.inject.Named;
 
 @UiController("carservice_City.browse")
 @UiDescriptor("city-browse.xml")
 @LookupComponent("citiesTable")
 @LoadDataBeforeShow
 public class CityBrowse extends StandardLookup<City> {
+    @Inject
+    private CollectionLoader<City> citiesDl;
+    @Install(to = "citiesTable.edit", subject = "afterCommitHandler")
+    protected void customersTableCreateAfterCommitHandler(City entity) {
+        citiesDl.load();
+    }
 }
