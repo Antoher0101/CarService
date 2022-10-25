d7a26fcc8e1f12747d96a195bf78889354fb7507
Antoher0101 <firelessball@yandex.ru>
2022-10-25 19:13:10 +0500

    Configured car service manager permissions

--- modules/core/src/com/company/carservice/role/CarServiceManager.java
+++ modules/core/src/com/company/carservice/role/CarServiceManager.java
@@ -1,6 +1,8 @@
 package com.company.carservice.role;
 
 import com.company.carservice.entity.CarService;
+import com.company.carservice.entity.Counterparty;
+import com.company.carservice.entity.Employee;
 import com.haulmont.cuba.security.app.role.AnnotatedRoleDefinition;
 import com.haulmont.cuba.security.app.role.annotation.*;
 import com.haulmont.cuba.security.entity.EntityOp;
@@ -12,18 +14,31 @@ import com.haulmont.cuba.security.role.ScreenPermissionsContainer;
 public class CarServiceManager extends AnnotatedRoleDefinition {
 
     @EntityAccess(entityClass = CarService.class, operations = {EntityOp.READ, EntityOp.UPDATE})
+    @EntityAccess(entityClass = Employee.class, operations = {EntityOp.CREATE, EntityOp.READ, EntityOp.UPDATE, EntityOp.DELETE})
+    @EntityAccess(entityClass = Counterparty.class, operations = {EntityOp.READ, EntityOp.UPDATE, EntityOp.DELETE})
     @Override
     public EntityPermissionsContainer entityPermissions() {
         return super.entityPermissions();
     }
 
-    @EntityAttributeAccess(entityClass = CarService.class, modify = "*")
+    @EntityAttributeAccess(entityClass = CarService.class,
+            modify = {"address", "counterparties", "phoneNumber", "employee"},
+            view = {"city", "creator"})
+    @EntityAttributeAccess(entityClass = Employee.class, modify = "*")
+    @EntityAttributeAccess(entityClass = Counterparty.class, modify = "*")
     @Override
     public EntityAttributePermissionsContainer entityAttributePermissions() {
         return super.entityAttributePermissions();
     }
 
-    @ScreenAccess(screenIds = {"application-carservice", "carservice_CarService.browse", "carservice_CarService.edit"})
+    @ScreenAccess(screenIds = {
+            "application-carservice",
+            "carservice_CarService.browse",
+            "carservice_CarService.edit",
+            "carservice_Counterparty.edit",
+            "carservice_Counterparty.browse",
+            "carservice_Employee.edit"
+    })
     @Override
     public ScreenPermissionsContainer screenPermissions() {
         return super.screenPermissions();
