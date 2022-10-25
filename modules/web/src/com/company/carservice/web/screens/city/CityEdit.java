6879a4693fc1a7fb1c460aec5e87e7801c29c6d6
Antoher0101 <firelessball@yandex.ru>
2022-10-25 21:34:54 +0500

    Implement realization of unchecking default city via dataManager

--- modules/web/src/com/company/carservice/web/screens/city/CityEdit.java
+++ modules/web/src/com/company/carservice/web/screens/city/CityEdit.java
@@ -2,9 +2,14 @@ package com.company.carservice.web.screens.city;
 
 import com.company.carservice.entity.City;
 import com.company.carservice.service.CityService;
+import com.haulmont.cuba.core.global.CommitContext;
+import com.haulmont.cuba.core.global.DataManager;
+import com.haulmont.cuba.core.global.LoadContext;
+import com.haulmont.cuba.gui.model.InstanceContainer;
 import com.haulmont.cuba.gui.screen.*;
 
 import javax.inject.Inject;
+import java.util.List;
 
 @UiController("carservice_City.edit")
 @UiDescriptor("city-edit.xml")
@@ -13,10 +18,24 @@ import javax.inject.Inject;
 public class CityEdit extends StandardEditor<City> {
     @Inject
     private CityService cityService;
+    @Inject
+    private DataManager dataManager;
+    @Inject
+    private InstanceContainer<City> cityDc;
 
     @Subscribe
     public void onBeforeCommitChanges(BeforeCommitChangesEvent event) {
         City editedCity = getEditedEntity();
-        cityService.resetDefaultCity(editedCity);
+        LoadContext<City> context = LoadContext.create(City.class).setQuery(
+                LoadContext.createQuery("select c from carservice_City c where c.defaultCity = true and c <> :changedCity")
+                        .setParameter("changedCity", editedCity));
+        List<City> list =  dataManager.loadList(context);
+        for (City city : list) {
+            city.setDefaultCity(false);
+        }
+        CommitContext commit = new CommitContext();
+        commit.setCommitInstances(list);
+
+        dataManager.commit(commit);
     }
 }
