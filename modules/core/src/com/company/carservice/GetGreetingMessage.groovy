dc364505af49a4105ced776520b55e9c654df35e
Antoher0101 <firelessball@yandex.ru>
2022-10-26 21:45:55 +0500

    Склонение слова 'лет' в поздравлении с днем рождения реализовано в groovy-скрипте

--- modules/core/src/com/company/carservice/GetGreetingMessage.groovy
+++ modules/core/src/com/company/carservice/GetGreetingMessage.groovy
@@ -2,8 +2,25 @@ package com.company.carservice
 
 import groovy.text.SimpleTemplateEngine
 
+static def plurals(n){
+    def ageLastNumber = n % 10;
+    def exclusion = (n % 100 >= 11) && (n % 100 <= 14);
+    def old = "";
+
+    if (ageLastNumber == 1)
+        old = "год";
+    else if(ageLastNumber == 0 || ageLastNumber >= 5 && ageLastNumber <= 9)
+        old = "лет";
+    else if(ageLastNumber >= 2 && ageLastNumber <= 4)
+        old = "года";
+    if (exclusion)
+        old = "лет";
+    return old;
+}
+binding.setVariable("year", plurals(getBinding().getVariable("age")))
+
 def template = 'Поздравляем вас с днем рождения, уважаемый $name $lastName!\n' +
-        'Желаем всего наилучшего в ваши $age лет!\n' +
+        'Желаем всего наилучшего в ваши $age $year!\n' +
         'С уважением, коллектив автосервиса \"$carServiceName\".'
 
 def engine = new SimpleTemplateEngine()
